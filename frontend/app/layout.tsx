09678d5799d3af1a9da3b90e4e2b8ec54a9ee288
Gisel G. <gisel@ggstudio.dev>
2026-04-09 00:27:04 -0700

    Various changes.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navigation from "./menu";
-// @ts-expect-error - allow side-effect CSS import without type declarations
-import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
